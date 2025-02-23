0cfe77e059686234320ab8658ee4982a245cd503
Bharath Dasaraju <bhrth.dsra1@gmail.com>
2025-02-24 05:43:42 +0800

    ML-Ops tools

---
+++ 1.ml-ops-tools/1.mlops-tools.sh
@@ -0,0 +1,28 @@
+Hyper Parameter tuning and optimizations:
+  1. Hyperopt
+  2. Optuna
+
+
+ML-Workflow orchestration
+  1. Airflow
+  2. Luigi
+
+
+ML Experiment Management Tools
+  1. mlflow
+  2. neptune.ai
+
+
+CD Tools for ML-Ops
+  1. kubeflow
+
+
+ML-ops code validation testing tools
+  1. Pytest
+  2. GreaterExpectations
+
+
+
+
+
+
