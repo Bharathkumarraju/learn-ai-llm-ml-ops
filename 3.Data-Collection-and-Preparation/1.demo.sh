bff50e887f4a7447f19c25ce6ecc54d048bd7aea
Bharath Dasaraju <bhrth.dsra1@gmail.com>
2025-02-24 10:05:27 +0800

    data collection and preparation for ML models to use it train the data

--- 3.Data-Collection-and-Preparation/1.demo.sh
+++ 3.Data-Collection-and-Preparation/1.demo.sh
@@ -1,1 +1,21 @@
 Data collection and preperation
+
+Collection relevant data to make informed decisions is the crux of any ML-Model.
+
+The more accurate and comprehemsive the data is the better the prediction will be.
+
+In Real-world scenarions the data is from different platforms
+like Spread sheets, SQL Databases and NoSQL databases..It is essential to centralise the data first and cleant it first,
+before using it in our ML models.
+
+Data collection and preperation for model training.
+
+how are we going collect the data and preprocess data before handing over to ML to train it.
+
+what are the methods available to collect and process the data.
+
+1. ETLs Data ingestions, Data lake, Feature store  and
+2. Data Preperations using spark, pandas etc...
+
+So before building ML models ... Data collection and Preperations plays vital role.
+
