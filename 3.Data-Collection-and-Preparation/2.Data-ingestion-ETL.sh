41d05252250dc5e49b0fa4ad6ab0578e64e1a30c
Bharath Dasaraju <bhrth.dsra1@gmail.com>
2025-02-25 02:13:03 +0800

    ETL is the first step in the MLOps journey

---
+++ 3.Data-Collection-and-Preparation/2.Data-ingestion-ETL.sh
@@ -0,0 +1,22 @@
+Where data stored in an organization
+
+  1. Customer Relationship Management(CRM) Systems - information about customers, interactions and sales opportunities
+  2. Marketing automation platforms
+  3. Social Media Platforms
+  4. Mobile app and website analytical data
+  5. Databases - Structured and unstructured data storage for analysis and reporting
+  6. IoT devices data
+  7. Google sheets, documents, images etc
+
+
+Now how can we consolidate all of the above information and store it in one place.
+So this process is called ETL - Extract, Transform and Load
+
+Extract -- Gathering raw data from DBs, APIs, Flatfiles - tools there like talent, DLT etc
+Transform -- cleaning, filtering and aggregating the data.
+Load -- Loading data into a target system like Data warehouse, Data lake and Repository.
+
+
+ETL is important i.e. to gather the required data for our modeling purpose.
+
+
