4afdccca9b878498a49baccd140f008b4d393da4
Bharath Dasaraju <bhrth.dsra1@gmail.com>
2025-02-28 07:47:24 +0800

    apache kafka and apache flink for real-time streaming

---
+++ 3.Data-Collection-and-Preparation/7.large-datasets-with-apacheSpark-and-Dask.sh
@@ -0,0 +1,8 @@
+So if we want to process the large data like TerraBytes of data we need Distributed data processing  - this is where Apache spark helps
+
+Apache Spark: Is a distributed computing for large-scale data processing and transformation.
+
+Apache spark processes millions of events per second for real-time insights.
+
+Apache spark is written in language called scala.
+
---
+++ 3.Data-Collection-and-Preparation/8.Realtime-streaming-datasets.sh
@@ -0,0 +1,8 @@
+Real-time streaming
+
+Apache Kafka and Apache Flink
+
+How to monitor machines which operates on 24/7 - using IoT Device - feed to Apache Kafka - Aggregate with Apache Flink
+
+
+
