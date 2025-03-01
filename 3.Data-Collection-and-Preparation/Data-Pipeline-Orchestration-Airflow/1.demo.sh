613d443f7dc4ff04a3618ac5dc975641a50a5d68
Bharath Dasaraju <bhrth.dsra1@gmail.com>
2025-03-02 06:08:44 +0800

    Data-Pipeline Orchestration using Apache Airflow

---
+++ 3.Data-Collection-and-Preparation/Data-Pipeline-Orchestration-Airflow/1.demo.sh
@@ -0,0 +1,11 @@
+Data Pipeline:
+  1. Scrapper Public Data
+  2. Store Data in S3
+  3. Transform the data
+  4. Store it in a presentation layer.
+
+Tools like Apache Airflow (and) perfect inorder to centralize the data pipline management
+
+ Apache Airflow: https://airflow.apache.org/
+ Perfect: https://www.prefect.io/
+
