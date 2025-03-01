3beefd9aeeed1f19e6bb4bd9bcab74bf36625ac0
Bharath Dasaraju <bhrth.dsra1@gmail.com>
2025-03-01 11:46:38 +0800

    difference between readitional Data-Lakes system and ML-Feature store

---
+++ 3.Data-Collection-and-Preparation/9.Feature-store.sh
@@ -0,0 +1,16 @@
+A game-changing component in the MLops-Landscape is feature store.
+
+Traditional systems like Data-Lakes pose challenges such as data latency, consistency, and reusability challenges for real-time ML applications.
+
+A real-time application like flight-booking service use feature-store to address those challenges
+
+a flight-booking application that uses real-time ML to recommend optimal ticket prices.
+
+Without a feature store, the system may rely on a slow batch data lake process, leading to outdated price predictions.
+With a feature store, real-time features such as:
+   Current flight demand
+   Weather conditions
+   Seat availability
+   Competitor pricing
+   User search history  are precomputed, stored, and served instantly to the ML model, ensuring low-latency, accurate recommendations.
+
