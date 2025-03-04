2467022dd6ff03f2d3a8146bb2ae89a3ccf7a6f0
Bharath Dasaraju <bhrth.dsra1@gmail.com>
2025-03-05 05:58:45 +0800

    MLOps and its deployment tools

---
+++ 5.Model-Deployment-and-Serving/4.Model-serving-tools.sh
@@ -0,0 +1,14 @@
+Powerful Model Serving, Model deployments Frameworks
+
+1. TensorFlow serving
+2. PyTorch (TorchServe)
+3. BentoML
+
+
+If we are working on tensorflow computer vision model that needs to process the thousands of image per second, Tensorflow serving is the goto choice.
+
+For PyTorch NLP models requiring A/B testing capabilities, ofcourse TorchServe is a goto choice - We can easily integrate TorchServe with Amazon Sagemaker or kubernetes for automated deployments.
+
+For teams needing Framework-agnostic solution with robust MLOps integration, BentoML fits perfectly.
+
+
