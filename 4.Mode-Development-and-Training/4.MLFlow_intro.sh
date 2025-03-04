0cb47fa812a01b14503cc639cf11f5c158af5b77
Bharath Dasaraju <bhrth.dsra1@gmail.com>
2025-03-04 20:40:41 +0800

    mlflow development lifecycle

--- 4.Mode-Development-and-Training/4.MLFlow_intro.sh
+++ 4.Mode-Development-and-Training/4.MLFlow_intro.sh
@@ -1,1 +1,10 @@
 mlflow - Managing ML experiments
+
+
+1. Model Selection - (Select from different machine learning models)
+2. Run experiments on the selected models
+3. Test the same-data with different ML Models
+
+The important step is visualizing the data and trying to understand which model is the best fit for this usecase.
+This is an iterative process - and this is where MLflow is actually helpful.
+
