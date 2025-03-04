4f5c5dedf2ba598f5838ba0da8cbe677fec5182a
Bharath Dasaraju <bhrth.dsra1@gmail.com>
2025-03-05 05:40:24 +0800

    model drift , online prediction and offline prediction

--- 5.Model-Deployment-and-Serving/2.deploy-mlmodel-api.sh
+++ 5.Model-Deployment-and-Serving/2.deploy-mlmodel-api.sh
@@ -1,4 +1,6 @@
 Deploy ML model with an micro-service architecture
 
-/predict api contains a ML-Model and will be deployed as micro-service
+/predict api contains a ML-Model and will be deployed as micro-service.. appliations /frontend api going to call this /predict API with parameters like branch_name and date :)
+
+
 
---
+++ 5.Model-Deployment-and-Serving/3.model_drift_offline_online_serving.sh
@@ -0,0 +1,11 @@
+When the model is ready in production , it is needed to be updated with new learning everytime.
+
+Consistently monitoring and retraing the model is necessary for the accurate prediction on the latest data.
+
+
+Online Serving:
+------------------->
+Whenever we get the new data(new feature) these data are getting updated to the feature-store, so based on this new input, Machine-Learning model continuously gets trained,This is called an online-prediction
+
+In the offline serving the new data getting updated to the feature store, but the machine learning model itself is getting trained in Batch Manner.
+
