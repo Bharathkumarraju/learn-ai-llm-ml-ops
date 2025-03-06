385bc0cebbee756a04bfbd28f86e997eafcebdb0
Bharath Dasaraju <bhrth.dsra1@gmail.com>
2025-03-07 06:24:40 +0800

    AWS Sagemaker IOT usecase

--- 8.AWS-Sage-Maker/1.demo.sh
+++ 8.AWS-Sage-Maker/1.demo.sh
@@ -1,1 +1,10 @@
 AWS Sagemaker for ML-ops
+
+MLOps Devlopment - Key Problems
+-----------------------------------------
+1. Lack of standardization
+2. Complexity in model deployment
+3. Tools overloaded  - Too many tools are needed to maintain the MLOps eco-system
+
+
+Amazon SageMaker - Build, Train, and deploy ML Models with fully managed insfrastructure and tools.
---
+++ 8.AWS-Sage-Maker/2.AWS-SageMaker-CoreComponents.sh
@@ -0,0 +1,23 @@
+AWS Sagemker comprises of wide-range of tools
+
+1. Amazon SageMaker Studio Lab - For experiments
+2. Notebook  - For development
+3. Amazon SageMaker ground truth - For labeling
+4. Canvas - For visualization
+5. Shadown Testig - for validation
+6. Train - training ml models
+7. Model
+
+
+1. SageMaker Studio an IDE  for ML with tools for data prep, model training, and deployment.
+2. SageMaker Notebooks  - Cloud-hosted JUpyter Notebooks
+3. SageMaker Training -  a managed service for scalable ML model training with various algorithms and frameworks.
+4. SageMaker INference - Simplify deployment process
+5. SageMaker Ground Truth - A data labeling service.
+
+6. SageMaker Autopilot - Automatically builds, trains and tunes ML models
+7. SageMaker Model Monitor
+8. SageMaker pipelines - CICD framework models
+9. SageMaker Feature Store  -
+10. SageMaker canvas - visualization toll inside AWS SageMaker
+
