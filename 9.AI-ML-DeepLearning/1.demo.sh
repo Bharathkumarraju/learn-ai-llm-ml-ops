2891084e20b0f454c8d392f7deff9795198b8fd0
Bharath Dasaraju <bhrth.dsra1@gmail.com>
2025-03-07 07:14:12 +0800

    all about AI ML and DL

--- 9.AI-ML-DeepLearning/1.demo.sh
+++ 9.AI-ML-DeepLearning/1.demo.sh
@@ -1,3 +1,26 @@
 AI - Artificial Intelligence
 ML - Machine Learning
 DL - Deep Learning
+
+Amazon SageMaker
+Amazon Rekognition
+
+Recommendations, Predictions and Forecasting AI and ML is necessary.
+
+AI:
+--->
+1. Visual Perception
+2. Speech recognition
+3. Decision-making
+4. Language translation
+
+AI ----> ML(Machine Learning), DL(Deep Learning)
+
+AI ---> Narrow AI / Generative AI
+
+ML: Train models to predict
+DL: Deep learning is a subset of ML that uses multi-layered neural-networks to model and solve more complex problems.
+
+
+
+
