36acd70a0f0b6bc44c348305367f2f36aef66653
Bharath Dasaraju <bhrth.dsra1@gmail.com>
2025-03-07 05:03:21 +0800

    Data Privacy and secuiry and Data Access Management - DAM

---
+++ 7.Data-Security-and-Governance/1.Data_Privacy_and_data_security.sh
@@ -0,0 +1,23 @@
+Data Privacy and Data Security
+
+HIPAA Compliance
+PCI Compliance
+
+Data Security:
+
+Technical measures
+Protocols
+
+
+Regulatory Compliance
+------------------------------------->
+GDPR: General Data Protection Regulation
+CCPA: California Consumer Privacy Act
+
+
+Data Security --> Encryption --> Access Controls --> Network Security
+
+
+REgular Audits
+Employee Training
+Incident Response plans
---
+++ 7.Data-Security-and-Governance/2.Data-Access-management.sh
@@ -0,0 +1,32 @@
+DAM - Data Access Management
+------------------------------------------->
+1. Automated Account Mapping
+2. On-Demand Permission Grants
+3. Governance Rule Application
+4. Unified Identity Verification
+5. Continuous Monitoring and insights
+6. Adaptive Access Control
+
+
+Key Components
+====================>
+1. Access Control Measures
+2. Data Classification
+3. Secure Data Handling
+4. User Training and Awareness
+
+Implementation
+---------------------->
+1. Implement robust policies
+2. Utilize access control models
+3. Automate identity Management
+4. Regular audits and monitoring
+
+
+Best Practices:
+------------------------------>
+1. Apply principle of Least privilege
+2. Regualry review permissions
+3. Implement comprehensive logging
+
+
