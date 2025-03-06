f05a7928283ca7c6cee1b1927d5c6e287ed7e598
Bharath Dasaraju <bhrth.dsra1@gmail.com>
2025-03-07 05:16:58 +0800

    General Data Protection Regualtion - GDPR

---
+++ 7.Data-Security-and-Governance/3.Data-Retention.sh
@@ -0,0 +1,26 @@
+1. Understanding Legal Requirements
+
+2. GDPR - it requires to delete the personal data if its no-longer required.
+
+3. Research Regulations
+
+4. Categorize the data
+
+
+Defining Retention Periods:
+=====================================>
+
+Fianancial data must needs to stay for 7 years.
+
+Implementing Secure Storage Solutions
+------------------------------------------->
+1. Utilize archiving tools
+2. Ensure the data security
+
+
+Establishing Precosures for Data Disposal
+
+Fostering INternal COllaboration
+
+Monitoring Compliance
+-------------------------->
---
+++ 7.Data-Security-and-Governance/4.Compliance-gdpr.sh
@@ -0,0 +1,8 @@
+GDPR, HIPAA, and PCI DSS - Overview
+
+The need for compliance:
+------------------------------------>
+Rights of data subjects
+
+GDPR(General Data Protection Regulation) Compliance in the EU regulates how personal data of individuals within the EU is processed and stored.
+
