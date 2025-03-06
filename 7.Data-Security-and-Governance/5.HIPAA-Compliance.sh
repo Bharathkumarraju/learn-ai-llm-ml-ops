c73e0181e1d9ac226f810044a1dec58a1eed6fe6
Bharath Dasaraju <bhrth.dsra1@gmail.com>
2025-03-07 05:34:37 +0800

    HIPAA - Health insurance portability and Accountability Act - PCI DSS - Payment Card Industry Data Security Standard

---
+++ 7.Data-Security-and-Governance/5.HIPAA-Compliance.sh
@@ -0,0 +1,11 @@
+HIPAA - Health INsurance  Portability and Accountability Act(HIPAA)
+
+Key components:
+----------------------->
+1. Privacy Rule  --> Establishes national standards for the protection of health information
+2. Security Rule --> Sets standards for the safeguarding electronic protected health information(ePHI)
+3. Breach Notification Rule
+
+MFA, encryptind patients data, access-controls i.e.
+Hospitals now use secure logins, access controls and encrypted databases.
+
---
+++ 7.Data-Security-and-Governance/6.PCI-DSS-Compliance.sh
@@ -0,0 +1,6 @@
+PCI DSS - Payment Card Industry Data Security Standard(PCI DSS)
+
+1. Build and Maintain a Secure network
+2. Protect Card-Holder Data
+3. Maintain a Vulnerability Management Program!
+
