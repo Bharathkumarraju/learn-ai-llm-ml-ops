5cf977265207f7e057ad36075b02d2256287bfed
Bharath Dasaraju <bhrth.dsra1@gmail.com>
2025-03-08 06:52:43 +0800

    structured vs unstructured data

--- 9.AI-ML-DeepLearning/2.types_of_inferencing.sh
+++ 9.AI-ML-DeepLearning/2.types_of_inferencing.sh
@@ -1,2 +1,5 @@
 Inferencing is the process in which AI models can make predictions using data
 
+Real-time inferencing
+Batch inferencing
+
---
+++ 9.AI-ML-DeepLearning/3.Data_types_in_AI_models.sh
@@ -0,0 +1,31 @@
+Numerical
+Categorical
+text
+images
+
+
+AWS Glue
+AWS Elastic Map Reduce
+Data wrangler
+
+Numerical Data:
+
+Amazon Sagamaker able to load numerical data-sets from s3, redshift etc...
+
+
+Categorical Data:
+
+Text Data and Natural Language Processing(NLP)
+
+Raw Text Data --> Tokenization, Stop-Word Removal(Pre-processing) ---> NLP model ----> Sentiment Analysis and Text Classification (Output)
+
+
+Image Data:
+
+Image Recognition, Object Detection and Facial Recognition
+Amazon Rekognition
+
+
+Audio Data:
+Speech Rekognition model
+
