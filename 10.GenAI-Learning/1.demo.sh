0047b38872980c84e4d9b57b913b48a3b8481dd8
Bharath Dasaraju <bhrth.dsra1@gmail.com>
2025-03-09 06:10:12 +0800

    AWS genAI:':

--- 10.GenAI-Learning/1.demo.sh
+++ 10.GenAI-Learning/1.demo.sh
@@ -1,1 +1,37 @@
-Generative AI - Tokens, Chunking, Bembeddings etc..
+Generative AI - Tokens, Chunking, embeddings etc..
+
+GenAI is transforming industries - It powers applications such as text generation, image creation etc...
+
+It is a Pre-trained Language Model.
+
+GenAI generates new content based on pre-trained modela...
+IT does prettymuch everything... text,images,audio,vedio and even code etc..
+
+genAI comprises of
+  1. Models
+  2. Transformers
+  3. Prompt Engineering
+  4. Inference
+  5. Context Window
+  6. Token
+  7. Vector
+  8. Embeddings
+  9. Chunking
+  10. Multi-Mode Models
+  11. Diffusion Models
+
+
+Transformer Network:
+------------------------>
+Context Window:
+Foundational Models  to Models like GPT and BERT
+
+Tokens and Tokenization:
+The process of breaking down input into Tokens
+
+Embeddings and Vectors:
+
+Chunking:
+
+
+
