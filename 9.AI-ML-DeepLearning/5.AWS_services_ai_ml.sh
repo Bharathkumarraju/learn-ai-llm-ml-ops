794206bcbff151041f07a04d76db32f781eee3b4
Bharath Dasaraju <bhrth.dsra1@gmail.com>
2025-03-09 05:05:18 +0800

    amazon pollt text to speech... speech to text

--- 9.AI-ML-DeepLearning/5.AWS_services_ai_ml.sh
+++ 9.AI-ML-DeepLearning/5.AWS_services_ai_ml.sh
@@ -1,1 +1,35 @@
-AWS
+AWS SageMaker - Simplify Machine Learning Development
+  1. Data Preperation
+  2. Model Training
+  3. Hyper parameter Tuning
+
+  Data Stored in S3 - AWS Glue for ETL
+
+
+AWS Polly - Pre-Trained model to enable quick ai integration
+AWS Lex - Pre-trained model to enable quick AI integration
+
+AWS Bedrock - premium GenAI service - Hosts AI models
+Guard Rails for Bedrock
+AWS Bedrock Agents
+
+
+Amazon Queue
+
+
+Amazon Rekognition: It analyzes images and videos for object detection, facial analysis and text recognition
+
+
+Amazon Lex: service for conversational AI - Amazon Alexa
+
+Amazon Polly - Audio interactions.. basically voice based system ...conversion for text to speech API
+SSML - Speech Synthetics Markup Language(SSML)
+
+
+
+
+
+
+
+
+
