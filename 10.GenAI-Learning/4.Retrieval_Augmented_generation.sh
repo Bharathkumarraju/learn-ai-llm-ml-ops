5fe7d5aca3a311509a2ae889d9d02880ee1f7fc5
Bharath Dasaraju <bhrth.dsra1@gmail.com>
2025-03-09 07:33:11 +0800

    Selecting the Pre-Trained AI models

---
+++ 10.GenAI-Learning/4.Retrieval_Augmented_generation.sh
@@ -0,0 +1,7 @@
+Amazon Bedrock uses RAGs to enhance foundational model performance in customer applications.
+
+Amazon Bedrock and Amazon Kendra USe Vector database to enhance foundation model performance in semantic search and document retrieval.
+
+
+Selecting the Pre-trained Models:
+
