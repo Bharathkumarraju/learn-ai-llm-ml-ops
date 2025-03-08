c1bb2589ed2c363ac5e93986b574752b3c86bc73
Bharath Dasaraju <bhrth.dsra1@gmail.com>
2025-03-09 07:10:25 +0800

    AI model metrics

---
+++ 10.GenAI-Learning/2.Foundation-model-lifecycle.sh
@@ -0,0 +1,25 @@
+Data Selection
+Model Selection
+Pre-Training
+Fine-Tuning
+Evaluating
+Deployment
+Feedback & Monitoring
+Iteration and Optimization
+
+Prompt Engineering
+
+User Prompt
+System prompts
+
+AWS INfra for genAI Apps:
+--------------------------------->
+SageMaker Jump start
+
+PartyRock  - Playground for testing genAI apps.
+
+Optimizing AI with Vector Databases,
+
+AWS Bedrock to host genAI Models
+
+AWS Bedrock for Generative AI development
---
+++ 10.GenAI-Learning/3.Foundational_models_apps.sh
@@ -0,0 +1,10 @@
+1. Convolutional Neural Network
+2. Recurrent Neural Network
+
+AI Model Performance Metrics:
+------------------------------------>
+1. Accuracy
+2. Precision
+3. Recall
+4. F1 Score
+
