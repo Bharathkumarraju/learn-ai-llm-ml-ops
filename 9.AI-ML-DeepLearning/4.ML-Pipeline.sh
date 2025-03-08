c1c0e81cfb25bd36b4d6cf5853e9bb4f5ca78b1a
Bharath Dasaraju <bhrth.dsra1@gmail.com>
2025-03-08 14:30:24 +0800

    ML model training using AWS Tools

--- 9.AI-ML-DeepLearning/4.ML-Pipeline.sh
+++ 9.AI-ML-DeepLearning/4.ML-Pipeline.sh
@@ -5,3 +5,30 @@ ML Model Lifecycle:
 3. Model Deployment
 4. Monitoring
 
+
+Hyper parameter tuning
+
+Amazon SageMaker have AMT(Automatic Model Training)
+
+Auto-optimize models by running multiple training jobs.
+
+Model Performance:
+----------------------->
+1. Accuracy
+2. Precision
+3. Recall
+4. F1 Score
+
+
+Model Deployment:
+------------------------->
+Real-time
+Batch
+
+Deploy using AWS SageMaker
+
+
+Model Monitor:
+------------------------->
+
+
