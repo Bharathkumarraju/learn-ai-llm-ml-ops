8bf10bdb78015628a6bbde3f48694b862bbe07f2
Bharath Dasaraju <bhrth.dsra1@gmail.com>
2025-03-08 13:56:40 +0800

    AI ML and DL

--- 9.AI-ML-DeepLearning/3.Data_types_in_AI_models.sh
+++ 9.AI-ML-DeepLearning/3.Data_types_in_AI_models.sh
@@ -29,3 +29,29 @@ Amazon Rekognition
 Audio Data:
 Speech Rekognition model
 
+Labeled Data vs Unlabeled Data:
+-------------------------------------->
+Labeled data contains both the input data and the corresponding output.
+
+unlaled data has the only input without the expected output.
+
+Time-series data
+Long shirt-term memory networks(LSTM)
+Prophet(a tool in aws)
+
+
+Balanced and imbalanced data.
+
+
+BigData and AI
+
+EMR, AWS Glue
+
+
+BigData -- Apache Hadoop  -- AWS EMP(ElasticMapReduce)
+
+Types of Machine Learning:
+1. Supervised learning
+2. Unsupervised learning
+3. Reinforcement Learning
+
