557a757514fa32ba759da81c628e62237dbadbfa
Bharath Dasaraju <bhrth.dsra1@gmail.com>
2025-03-09 13:20:53 +0800

    How RAG works

--- 10.GenAI-Learning/4.Retrieval_Augmented_generation.sh
+++ 10.GenAI-Learning/4.Retrieval_Augmented_generation.sh
@@ -5,3 +5,14 @@ Amazon Bedrock and Amazon Kendra USe Vector database to enhance foundation model
 
 Selecting the Pre-trained Models:
 
+RAG(Retrieval Augmented Generation) is a method that basically combines two components i.e.
+Large language model generation and information Retrieval.
+
+
+Basically it take Large Language model and it retrieves information.
+
+So the goal of RAG is to basically retrieve that information and ingested into the model.
+
+
+
+
