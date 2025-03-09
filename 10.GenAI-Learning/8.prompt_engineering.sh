ebca94a3c26023df74c3e88fe2b9d3e5ea5f8d94
Bharath Dasaraju <bhrth.dsra1@gmail.com>
2025-03-10 07:16:21 +0800

    Pre-training and fine-tuning foundational models

---
+++ 10.GenAI-Learning/8.prompt_engineering.sh
@@ -0,0 +1,14 @@
+Prompt engineering provides certain inputs to model and tells it what to do with the user inputs.
+
+Prompt Templates
+Negative Prompts
+Context in prompts
+
+
+Pre-Training and Fine-tuning Foundational Models
+
+PEFT - parameter Efficient Fine-Tuning
+LoRA - Low-Rank Adaption
+ReFT - Reperesentaion Fine-Tunning
+
+Multitask Fine-Tuning
