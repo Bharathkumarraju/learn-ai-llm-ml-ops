e688496efc22d1eb17851fbb239a816f5d78a679
Bharath Dasaraju <bhrth.dsra1@gmail.com>
2025-03-10 06:16:03 +0800

    Vector embeddinh support for AWS services

---
+++ 10.GenAI-Learning/5.vector_databases.sh
@@ -0,0 +1,29 @@
+Vector Databases store data as embeddings, which are numerical representations of data like text and images.
+
+These embeddings allow fast, efficient and semantically relevant searches for AI and machine learning tasks.
+
+Several AWS Services help store and manage embeddings in vector databases.
+
+
+Amazon OpenSearch service for Generative AI.
+k-Nearest Neighbors(k-NN) for the efficient queries.
+
+
+Amazon Aurora PostgreSQL-Compatible Edition and Amazon RDS for PostgreSQL support pgvector
+1. pgvector extension available on Amazon Aurora and Amazon RDS for PostgreSQL.
+2. Enables storage and similarity searches using ML-generated embeddings
+3. Embeddings capture semantic meaning from text processed by large language models(LLMs)
+
+Amazon Neptune ML:
+-------------------------->
+Uses Graph Neural Networks(GNNs) to enhance predictions using complex graph relationships.
+
+Vector Search for amazon memoryDB
+
+Vector search by Amazon DocumentDB(with MongoDB compatibility)
+
+RAG with Amazon Bedrock and Custom Knowledge bases.
+
+RAG combines retrieved data with generative models.
+Amazon Bedrock supports RAG by integrating with custom knowledge bases.
+
