4abae95bbe97176e7aea1cc041ea65b3550aa201
Bharath Dasaraju <bhrth.dsra1@gmail.com>
2025-03-11 07:06:21 +0800

    Security in AI

--- 10.GenAI-Learning/11.Security_compliance_governance.sh
+++ 10.GenAI-Learning/11.Security_compliance_governance.sh
@@ -13,3 +13,26 @@ SageMaker Model Registry for Model versioning
 SageMaker Model cards  - Documentong model deails
 
 SageMaker Feature Store
+
+AWS Artifact: Simplifying Compliance Reporting
+AWS Glue DataBrew: Data Preparation for Governance
+AWS Lake Foramtion
+Amazon S3
+Amazon SageMaker Clarify
+AWS Config - Continuous Monitoring for Compliance
+AWS Inspector - Security and Compliance Assessment
+AWS Audit Manager - Streamlined compliance Auditing
+AWS Cloudtrail - all API calls
+AWS Trusted Advisor - Best practices and compliance recommendations
+
+
+Data Governance Strategies:
+
+Data Lifecycle management
+- s3 lifecycle management
+
+Data logging -- AWS Cloud Trail, Amazon cloudwatch
+
+Data Curation and understanding -- AWS Glue DataBrew
+
+Master DAta Management  using Amazon Redshift and AWS Glue
