2dd31d3b361d1c0bb04e927c6377ca851cc32b8b
Bharath Dasaraju <bhrth.dsra1@gmail.com>
2025-03-11 05:59:27 +0800

    Sagemaker model registry, model cards and Feature store

--- 10.GenAI-Learning/11.Security_compliance_governance.sh
+++ 10.GenAI-Learning/11.Security_compliance_governance.sh
@@ -1,1 +1,15 @@
 security
+
+SageMaker notebook instance in a private subnet
+
+SageMaker Distributed Training - Inter-mode-encryption
+
+Amazon SageMaker Security
+
+
+Data Source --> Data Processin --> Data Storage
+
+SageMaker Model Registry for Model versioning
+SageMaker Model cards  - Documentong model deails
+
+SageMaker Feature Store
