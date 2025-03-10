305e660a6dc2403923134d298ece5d8d7df7f68b
Bharath Dasaraju <bhrth.dsra1@gmail.com>
2025-03-11 05:26:51 +0800

    Security, Compliance and Governance:

--- 10.GenAI-Learning/10.responsible-ai.sh
+++ 10.GenAI-Learning/10.responsible-ai.sh
@@ -2,4 +2,43 @@ Healthcare
 Financial
 Law Firms
 
-E
+Governance
+Security
+robustness
+explainability
+fairness in AI
+
+Tools for identifying responsible AI:
+----------------------------------------------->
+1. Amazon SageMaker Clarify  -- Bias Detection, Model Decisions with SageMaker Clarify
+2. Amazon Bedrock for Guardrails
+3. Environmental impact in assessment of AI
+
+
+Data PRivacy and security risks:
+
+
+Balaned datasets
+
+AMazon Sagemaker clarify
+
+SageMaker Data Wrangler
+
+Data Preprocessing:
+------------------------------>
+1. Data CLeaning
+2. Normalization
+3. Feature Selection
+
+REgualr auditing and fairness
+
+Transparency and explainable ai models
+----------------------------------------->
+
+Human-Centered AI
+
+A2I - Amazon Augmented AI
+
+Reinforcement Learning from Human Feedback (RLHF)
+
+Human Centered Design(HCD)
