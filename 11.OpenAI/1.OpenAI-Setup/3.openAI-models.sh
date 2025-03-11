050fcfb05e3d70b0f12b8da36f81d3392fc570eb
Bharath Dasaraju <bhrth.dsra1@gmail.com>
2025-03-12 06:21:23 +0800

    GPT-4 has been built in transformer architecture

---
+++ 11.OpenAI/1.OpenAI-Setup/3.openAI-models.sh
@@ -0,0 +1,18 @@
+GPT-4  -- Generative Pre-trained Transformer-4
+DALL-E
+Whisper
+Text to Speech(TTS)
+Embedding
+Moderation
+
+
+Generative Pre-trained Transformer-4(GPT-4) performs wide variety of tasks.. such as
+1. Human-like text generation
+2. language translation
+3. Summarization
+4. Answering questions
+5. Conversational ai
+
+GPT-4 developed on the Transformer Architecture.
+
+
