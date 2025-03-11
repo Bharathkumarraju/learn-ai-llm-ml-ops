280e690a1f2c283543b6689ce57a73f41b9609d2
Bharath Dasaraju <bhrth.dsra1@gmail.com>
2025-03-12 04:38:17 +0800

    AI, ML, LLM, GenAI, Neural networks and training models

---
+++ 11.OpenAI/1.OpenAI-Setup/1.How-OpenAI-works.sh
@@ -0,0 +1,23 @@
+1. Machine Learning(ML) & Artificial Intelligence(AI)
+ ML - Learn from data without explicitly being programmed, identify what comes next in the data - i.e. pattern recognition
+ AI - Performs complext tasks like only humans can do, such as reasoning, making decisions or solving problems ..ex: Generation, language understanding, code generation
+
+2. LargeLanguage Models
+  LLM- Its AI sysem trained in vast data-systems...
+  LLMs take in a prompt input text, and use probabilities to predict the next word, or sequence of words. THis prediction is based-on patterns learned during training.
+  1. Prompt Engineering
+  2. RAG
+  3. Fine-Tuning
+  LLM Optimization - how the model needs to act
+  Contect Optimization - what the model needs to know
+
+
+3. Generative AI(GPT-4 & DALL-E)
+
+
+4. Built on Neural Networks
+
+
+5. Training Models
+
+
