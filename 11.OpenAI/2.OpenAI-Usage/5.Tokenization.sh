100576718c4df60d3eec36be5519d7505e13c9fa
Bharath Dasaraju <bhrth.dsra1@gmail.com>
2025-03-13 05:11:06 +0800

    Tokenization in AI models

---
+++ 11.OpenAI/2.OpenAI-Usage/5.Tokenization.sh
@@ -0,0 +1,9 @@
+in LLM tokenization converts raw text into smaller units called tokens
+
+LLM models can only process the tokens not the raw text
+
+tokens standardizes the way to represnt the input in the form of vector and embeddings
+
+Word level tokenization
+Sub-word level tokenization
+character level tokenization
