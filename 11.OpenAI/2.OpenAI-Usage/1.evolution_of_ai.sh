91b6b81be5e93f385a6dad5bf0756e0b9b9f306b
Bharath Dasaraju <bhrth.dsra1@gmail.com>
2025-03-12 21:40:32 +0800

    evolution of ai transformers in genAI

--- 11.OpenAI/2.OpenAI-Usage/1.evolution_of_ai.sh
+++ 11.OpenAI/2.OpenAI-Usage/1.evolution_of_ai.sh
@@ -6,3 +6,12 @@ Rule-based systems --> Machine Learning
 
 
 Deep-Learning --> Neural network and Natural language processing.
+
+
+The convergence of AI and data:
+
+
+AGI - Artificial General Intelligence
+
+
+Transformers how they power Generative-AI
