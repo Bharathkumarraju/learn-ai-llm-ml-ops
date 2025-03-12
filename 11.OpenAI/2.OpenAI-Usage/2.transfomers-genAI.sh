18febea5843cc1c432fd61ede08d2478f736505c
Bharath Dasaraju <bhrth.dsra1@gmail.com>
2025-03-13 04:04:33 +0800

    Attention Mechanism and Pre-training and fine-tuning

--- 11.OpenAI/2.OpenAI-Usage/2.transfomers-genAI.sh
+++ 11.OpenAI/2.OpenAI-Usage/2.transfomers-genAI.sh
@@ -1,2 +1,20 @@
 Transformers training data efficiently.
 
+Attention mechanism in transformers
+
+
+Attention score
+
+Q - Query
+K - Key
+V - Value
+
+Multi-Head attention
+
+how attention mechanison works
+--------------------------------------------->
+Step1: Embedding input --> Vectors
+Step2: Query, Key, Value
+Step3: Attention Score
+Step4: Weighted sum
+
---
+++ 11.OpenAI/2.OpenAI-Usage/3.Pretraining-finetuning-LLMs.sh
@@ -0,0 +1,15 @@
+Pre-Training and Fine-Tuning
+
+GPT-4 pre-trained on large datasets.
+
+Sentiment analysis, summarization and translation
+
+Pre-training:
+----------------------------->
+1. Initial stage where LLMs gets trained
+2. Learns general patterns, structure, relationships
+
+Fine-Tuning:
+-------------------------->
+1. Pre-Trained model is adapted to perform a specific task
+2. Uses smaller, task-specific dataset
