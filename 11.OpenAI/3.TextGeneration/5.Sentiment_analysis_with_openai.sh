de3753fb13a0354f6fce2cb945646840ed370032
Bharath Dasaraju <bhrth.dsra1@gmail.com>
2025-03-14 05:08:41 +0800

    Sentiment analysis with openai

--- 11.OpenAI/3.TextGeneration/5.Sentiment_analysis_with_openai.sh
+++ 11.OpenAI/3.TextGeneration/5.Sentiment_analysis_with_openai.sh
@@ -1,6 +1,14 @@
 (.venv) bharathkumardasaraju@3.TextGeneration$ python3.12 ./sentiment-analysis.py
-Input: I love the new design of the product. It's amazing!
+Input: I love the new design of the product. Its amazing!
 Sentiment: The sentiment of the text is positive. The user expresses love for the new design of the product and describes it as amazing.
+(.venv) bharathkumardasaraju@3.TextGeneration$
+
+
+(.venv) bharathkumardasaraju@3.TextGeneration$ python3.12 ./sentiment-analysis.py
+Input: I love the new design of the product. Its amazing!
+Sentiment: The sentiment of the text is Positive.
+Input: The product works well, but the customer service was awful.
+Sentiment: Negative
 (.venv) bharathkumardasaraju@3.TextGeneration$
 
 
