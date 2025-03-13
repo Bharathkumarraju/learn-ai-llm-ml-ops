00e194341dfd611c8f5f48d907ada8ca8c419974
Bharath Dasaraju <bhrth.dsra1@gmail.com>
2025-03-13 21:46:04 +0800

    Translate Article

--- 11.OpenAI/3.TextGeneration/demos/4.translate_article.sh
+++ 11.OpenAI/3.TextGeneration/demos/4.translate_article.sh
@@ -1,2 +1,7 @@
 Translating Article
 
+(.venv) bharathkumardasaraju@demos$ python3.12 ./translate_article.py
+All human beings are born free and equal in dignity and rights. They are endowed with reason and conscience and should act towards one another in a spirit of brotherhood.
+(.venv) bharathkumardasaraju@demos$
+
+
