a6cd0e8e2090dfffd989088d995e543348c45de0
Bharath Dasaraju <bhrth.dsra1@gmail.com>
2025-03-15 04:10:06 +0800

    how embeddings converts text, docs, images and audio into embeddding ... basically groups of floating point numbers

--- 11.OpenAI/3.TextGeneration/embedding_example.py
+++ 11.OpenAI/3.TextGeneration/embedding_example.py
@@ -1,1 +1,14 @@
 # Embedding example
+
+import os
+from openai import OpenAI
+
+api_key = os.getenv("OPENAI_API_KEY")
+client = OpenAI(api_key=api_key)
+
+response = client.embeddings.create(
+    input="Upon Moana2 release, critics complimented that the grpahics are outstanding!",
+    model="text-embedding-3-small"
+)
+
+print(response.data[0].embedding)
