e318d5f08fa944675d5c87e49fdc7a9b858ad6ad
Bharath Dasaraju <bhrth.dsra1@gmail.com>
2025-03-16 03:34:17 +0800

    AZURE ML and AI services

--- 12.Azure-AI-Cert/1.demo.sh
+++ 12.Azure-AI-Cert/1.demo.sh
@@ -1,2 +1,9 @@
 AZURE AI Practioner
 
+
+1. Artificial Intelligence Workloads & Considerations
+2. Fundamental Principles of Machine Learning on AZURE
+3. Feature of Computer vision workloads on Azure
+4. Features of Natual Language Processing (NLP) workoads on Azure
+5. Features of Generative AI workloads on Azure
+
