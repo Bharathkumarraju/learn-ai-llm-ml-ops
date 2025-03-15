1fa6e4bc498ced3dcab88d4851814691ac9850b2
Bharath Dasaraju <bhrth.dsra1@gmail.com>
2025-03-16 06:14:29 +0800

    Azure AI services

--- 12.Azure-AI-Cert/2.Azure-AI/1.Azure-AI.sh
+++ 12.Azure-AI-Cert/2.Azure-AI/1.Azure-AI.sh
@@ -2,3 +2,44 @@ Azure blob storage
 
 Azure Data lake storage
 
+
+Azure compute - Azure VM - Azure AKS - Azure Functions
+
+
+AI Services in Azure
+
+Azure Machine Learning:
+------------------------------------------->
+Model Training, Deploying and Management
+
+
+Azure AI Services:
+---------------------------------------->
+Vision
+Speech
+Language
+Decison-making
+Generative AI
+
+Azure Cognitive Search:
+------------------------------------------->
+Data extraction
+Data Enrichment
+Data indexing
+Enhances the search experience
+Facilitates Knowledge discovery through effective data mininng techniques.
+
+
+
+
+Deploy Standalone Resources for specifci services. like vision, speech, language and decision-making
+
+General AI Services Resource for multiple services. - A unified resource that supports various AI services.
+
+
+
+
+
+
+
+
