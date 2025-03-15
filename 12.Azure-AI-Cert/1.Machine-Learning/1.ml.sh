cae4e3c5c192a74f5d5e964e7f0674984610a6c7
Bharath Dasaraju <bhrth.dsra1@gmail.com>
2025-03-16 04:04:44 +0800

    what ML

---
+++ 12.Azure-AI-Cert/1.Machine-Learning/1.ml.sh
@@ -0,0 +1,26 @@
+learn from data -ML
+
+Supervised Learning
+Unsupervised Learning
+Reinforcement Learning
+
+
+
+Deep Learning  - Neural Networks
+
+
+Azure ML
+
+ML:
+--->
+ML is about creating predictive models by finding patterns in data.
+This process involves two main phases - Training and Inferencing
+
+1. Training Data - Historical data
+   1. Labels and Features
+   2. Algorithm
+   3. Model
+
+2. Inferencing
+  1. Train the Model
+  2. Prediction
