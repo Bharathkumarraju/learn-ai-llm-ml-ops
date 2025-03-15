44c106b1e3d0f394269e902b08ae8e3b6de274af
Bharath Dasaraju <bhrth.dsra1@gmail.com>
2025-03-16 03:50:58 +0800

    All about AI principles

--- 12.Azure-AI-Cert/1.demo.sh
+++ 12.Azure-AI-Cert/1.demo.sh
@@ -7,3 +7,35 @@ AZURE AI Practioner
 4. Features of Natual Language Processing (NLP) workoads on Azure
 5. Features of Generative AI workloads on Azure
 
+AI usecase:
+-------------------->
+1. Prediction and Pattern Recognition
+2. Anomaly detection and decision making
+3. Visual interpretation
+4. Understanding human language and Conversation -(Natural Language Processing - NLP)
+5. Information Extraction
+
+
+AI Workloads:
+------------------------>
+1. Machine Learning
+2. Computer Vision
+3. Natual Language Processing - NLP
+4. Document Intelligence
+5. Knowledge Mining
+6. Generative AI
+
+Principles of Responsible AI
+------------------------------------------------->
+1. Fairness - need to make unbaised decisions
+2. Reliability and Safety - ex: Autonomous vehicles and health care
+3. Privacy and security
+4. Inclusiveness
+5. Transparency
+6. Accountability
+
+
+
+
+
+
