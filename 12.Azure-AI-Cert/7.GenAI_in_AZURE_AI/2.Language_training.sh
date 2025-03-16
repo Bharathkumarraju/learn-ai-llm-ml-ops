83e3ce702cd77747ee600eaa342dfc016072579c
Bharath Dasaraju <bhrth.dsra1@gmail.com>
2025-03-16 20:54:34 +0800

    Azure copilot

--- 12.Azure-AI-Cert/7.GenAI_in_AZURE_AI/2.Language_training.sh
+++ 12.Azure-AI-Cert/7.GenAI_in_AZURE_AI/2.Language_training.sh
@@ -8,3 +8,10 @@ Tokenization breaks sentence down into numbers that the model can process.
 Embeddings helps the model to understand the relationship between words by positioning them in a meaning based space.
 Attention allows the model to focus on the most relevant words when making predictions
 
+
+Founadational Model
+
+Pre-trained Foundational model
+
+
+
---
+++ 12.Azure-AI-Cert/7.GenAI_in_AZURE_AI/3.Copilot.sh
@@ -0,0 +1,26 @@
+Generative AI-Powered Assistants integrated into applications often as chat interfaces.
+
+Copilots helps contextual assistance in everyday tasks.
+
+
+
+Extend AI Copilot with custom integration.
+Develop personalized copilots.
+
+Copilot services offered by Microsoft:
+------------------------------------------------------------->
+1. Daily Tasks
+2. Business Processes
+3. INfra, security and software development
+
+PowerBI
+
+github copilot
+
+Effective prompting is all about ---> clarity, context and specificity
+
+Copilot is AI-powered Assistant
+
+
+Copilot Studio and Azure AI Studio
+
