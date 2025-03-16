13b374923e9e37dcd9e238eea753f6649c6c14b3
Bharath Dasaraju <bhrth.dsra1@gmail.com>
2025-03-17 05:26:33 +0800

    Azure OpenAI Services'

---
+++ 12.Azure-AI-Cert/8.Azure_OpenAI/1.Azure_OpenAI.sh
@@ -0,0 +1,38 @@
+Azure OpenAI service designed specifically for deploying, customizing and hosting large language models.
+
+
+Azure OpenAI Provides
+1. Pre-Trained AI models
+2. Model Customization
+3. Responsible AI tools
+4. Enterprise security features
+
+Azure AI Studio
+REST API
+Supported SDKs
+Azure CLI
+
+
+Models Supported AzureOpenAI
+
+GPT-4o and GPT-4 Turbo
+GPT-3.5
+Embeddings model
+DALL-E
+Whisper - speech recoginition model
+
+
+Capabilities of Azure OpenAI
+----------------------------------------------------->
+
+Azure OpenAI Studio
+1. Model Deployment
+2. Generatibe AI
+3. Supported Models
+
+OpenAI Playgrounds
+1. Experimentation
+2. Configuration
+
+GPT - Generative Pre-trained transformer models
+
