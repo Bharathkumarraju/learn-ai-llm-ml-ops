de62d42152b114d98feace239b82a519201fbc1b
Bharath Dasaraju <bhrth.dsra1@gmail.com>
2025-03-16 08:46:36 +0800

    Azure AI speech service

---
+++ 12.Azure-AI-Cert/4.Azure_NLP/2.NLP_in_Azure.sh
@@ -0,0 +1,20 @@
+NLP:
+--------------------->
+
+1. Language
+lanaguage detection
+key phrase extraction
+personal info detection
+conversational lanaguage understandinn
+
+2. Speech
+text to speech
+speech to text
+speech translation
+language identification
+
+3. Translation
+text translation
+Document translation
+custom translation
+
