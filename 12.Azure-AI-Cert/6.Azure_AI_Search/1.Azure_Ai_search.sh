1bf8a42f09e1e2a19892b0df139a857fcc75dc8b
Bharath Dasaraju <bhrth.dsra1@gmail.com>
2025-03-16 11:55:26 +0800

    azure AI search and AI Enrichment

---
+++ 12.Azure-AI-Cert/6.Azure_AI_Search/1.Azure_Ai_search.sh
@@ -0,0 +1,47 @@
+how to extract the vast amounts of data.
+
+Azure AI Search services  - How Azure AI helps to build intelligent search solutions
+
+AI ENrichment
+
+Azure AI Search INdex
+
+
+
+Data Mining:
+----------------->
+Search and bots
+
+
+Azure AI Search Services:
+--------------------------------->
+
+ingest:
+------------>
+1. Azure blob storage containers
+2. Azure SQL Database, Documents in Cosmos DB
+3. Azure Data Lake Storage Gen2
+4. Azure Table Storage - Store structured NoSQL data
+
+
+AI Enrichment and Index:
+----------------------------->
+
+Explore:
+------------>
+Search performed on INdexes
+
+
+AI search service in .... Azure AI services.
+
+
+
+AI ENrichment:
+------------------------->
+skillsets - identfies entities in text
+Evaluates Sentiment
+Translate text
+
+
+
+
