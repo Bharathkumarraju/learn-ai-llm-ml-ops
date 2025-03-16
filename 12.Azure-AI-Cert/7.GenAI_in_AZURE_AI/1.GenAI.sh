105357f7c4c10644881d95f5d511494e29ddb1ca
Bharath Dasaraju <bhrth.dsra1@gmail.com>
2025-03-16 20:23:31 +0800

    Tokens, EMbeddings and Attention

---
+++ 12.Azure-AI-Cert/7.GenAI_in_AZURE_AI/1.GenAI.sh
@@ -0,0 +1,26 @@
+Large Language Model:
+--------------------------------->
+1. Natural Language generation
+2. Image Generation
+3. Code Generation
+
+
+Language Models:
+-------------------------->
+How AI been trained to make decisions like human is like this ---
+
+AI uses transformer model. It has two parts, the Encoder and Decoder.
+
+First we feed the model with large amount of data. This could be from book, scientific papers that helps the model to learn language patterns.
+
+Then we feed this data to transformer model -
+
+So the encoder will take these words and converts them into floating pointing numbers, which are known as embeddings.
+Each word that means each embeddings gets mapped to as set of coordinates in a multi-dimensional space.
+
+Now that the model has learned from all this data, It can start making predictions.
+
+So when user give it a prompt,
+Like ----  When my bird was  the decoder uses the embeddings and patterns it learned during the training  to generate a natural language sequence -
+So in this case, it might suggest chirping as completion.
+
---
+++ 12.Azure-AI-Cert/7.GenAI_in_AZURE_AI/2.Language_training.sh
@@ -0,0 +1,10 @@
+Language training done in 3 steps
+
+1. Tokenization
+2. Embeddings
+3. Attention
+
+Tokenization breaks sentence down into numbers that the model can process.
+Embeddings helps the model to understand the relationship between words by positioning them in a meaning based space.
+Attention allows the model to focus on the most relevant words when making predictions
+
