6c9bb3746dd15abeee2470d161eb6e75c47cf1cd
Bharath Dasaraju <bhrth.dsra1@gmail.com>
2025-03-16 08:37:21 +0800

    NLP - Natural language Processing

---
+++ 12.Azure-AI-Cert/4.Azure_NLP/1.Azure_Natural_Language_Processing.sh
@@ -0,0 +1,18 @@
+Azure Natural Language Processing(NLP)
+
+AN AI mechanism...
+
+NLP focuses on helping computers to understand, interpret and respond to human language in a meaningful way.
+
+
+Raw Text
+
+Raw Text  preprocessing  --> tokenization  --> Training --> Language Model
+
+
+Text Analysis
+Opinion Mining
+Machine Translation
+Summarization
+ConversationalAI (chatbots)
+
