31a012992ea0ceab8e7960c255f5849e56d4ae5e
Bharath Dasaraju <bhrth.dsra1@gmail.com>
2025-03-16 11:14:38 +0800

    Azure AI Document Intelligence

---
+++ 12.Azure-AI-Cert/5.Azure_Document_intelligence/1.Azure_AI_document_intelligence.sh
@@ -0,0 +1,14 @@
+Form Analysis using document intelligence
+
+Document Intelligence Studio
+
+Document Analysis
+Pre-Built models for
+1. INvoices
+2. REceipts
+3. ID Documents
+
+Custom-models:
+custom models with sample data.
+Field identification
+
