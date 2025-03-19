c01f459ffe767c04b187dd289a1d1897ac08122a
Bharath Dasaraju <bhrth.dsra1@gmail.com>
2025-03-20 05:30:21 +0800

    azure AI fundas

--- 14.Azure_GenAI_LLM_RAG/2.GenAI.sh
+++ 14.Azure_GenAI_LLM_RAG/2.GenAI.sh
@@ -7,7 +7,7 @@ Audio Modality
 
 
 Nueral Networks
---------------------------------------------------->
+------------------------------------------------------------>
 Input Layer --> Hidden Layers --> Output Layer
 
 
@@ -20,4 +20,24 @@ AI Applications on Azure
 LLMOps and Lifecycles of LLM -
 
 Advanced RAG
+
+
+-------------------------------------------------------------------------------------------------------------------->
+
+
+Imperative programing  to declarative programing
+
+Paradigm Shift  --->
+01. Supervised Learning  (like school with children - you need to show multiple examples of cat and dog and allow them to learn)
+
+02. Unsupervised Learning ( its like throwing a baby in an unknown environment asking to figure it out on his own - we didnt give examples )
+
+03. Reinforcement Learning ( train the system - feedback loop )
+
+
+
+
+
+
+
 
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # learn-ai-ml-llm-ops
-Machine-Learning , Gen AI Models Development and Deployment
+GenAI , LLM(Large Language Models) and Machine-Learning(ML) Models Development and Deployment
+
+
+## Azure AI Certification - AI 900 AI fundamentals
+
+![Bharaths Azure-AI900](./13.AWS-AI-Cert/Azure_AI_Fundamentals.png)
