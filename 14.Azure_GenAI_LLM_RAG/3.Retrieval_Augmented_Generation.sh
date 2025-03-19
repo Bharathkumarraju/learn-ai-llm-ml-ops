24d2a0071a5d499f8e799bc8e936495fd59032c4
Bharath Dasaraju <bhrth.dsra1@gmail.com>
2025-03-20 07:38:30 +0800

    Agentic RAG

--- 14.Azure_GenAI_LLM_RAG/3.Retrieval_Augmented_Generation.sh
+++ 14.Azure_GenAI_LLM_RAG/3.Retrieval_Augmented_Generation.sh
@@ -4,3 +4,7 @@ Retrieval Augmented Generation(RAG)
 
 RAG is a powerful hybrid architecture that extends the capabilities of Foundation models by integrating real-time data retrieval from external databases.
 
+Agentic RAG
+
+the process of which external data-source needs to be selected thats where different types of RAG comes into the picture
+
