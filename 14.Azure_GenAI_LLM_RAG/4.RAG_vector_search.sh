7ac824e167d33f166733e9af986de66c62ddf159
Bharath Dasaraju <bhrth.dsra1@gmail.com>
2025-03-20 08:11:07 +0800

    Vector search in 3D space

---
+++ 14.Azure_GenAI_LLM_RAG/4.RAG_vector_search.sh
@@ -0,0 +1,8 @@
+Vector search basically is that...
+
+it searches the based on the semantic meaning  of the words...
+
+lets say in your Database for all the cars you used the word called vehicle....but IN normal search technique if you search for cars then you want be able to get anything
+
+but if its in Vector Search it is possible to het the
+
