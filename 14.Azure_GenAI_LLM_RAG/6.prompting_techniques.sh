80c941de6f52d5982734e2f7aeffb35399c4d4fe
Bharath Dasaraju <bhrth.dsra1@gmail.com>
2025-03-22 09:03:53 +0800

    Presence and Frequency penalties

--- 14.Azure_GenAI_LLM_RAG/6.prompting_techniques.sh
+++ 14.Azure_GenAI_LLM_RAG/6.prompting_techniques.sh
@@ -1,3 +1,34 @@
 1. Fine-Tuning
 2. Conext Learning
 
+
+1. Zero-shot prompting -  Asking the model to perform task directly without giving any examples
+
+2. Few-Shot prompting - Providing a few examples before asking the model to questions
+
+3. Chain-of-thought prompting - OpenAIo1
+
+4. Adversarial prompting -
+
+Anthropics Approach with Claude
+
+Prompt Management:
+----------------------------------------->
+1. Prompt Recording
+2. Feedback Mechanism
+3. Templates
+
+
+5. Inference parameters: - Mastering the output
+
+Hyper-Parameter Tuning
+
+temperature:
+Top-K sampling
+Top-P Sampling
+Presence and Frequency Penalties
+Logit bias
+
+
+
+
