f43763126b9a242696a3cea58b2b4ffaafd8745a
Bharath Dasaraju <bhrth.dsra1@gmail.com>
2025-03-22 16:00:26 +0800

    LLMOps

--- 14.Azure_GenAI_LLM_RAG/7.LLMs_performance.sh
+++ 14.Azure_GenAI_LLM_RAG/7.LLMs_performance.sh
@@ -14,3 +14,26 @@ https://huggingface.co/spaces/lmarena-ai/chatbot-arena-leaderboard
 
 https://docs.ragas.io/en/stable/
 
+
+Learning Interprertability Tool:
+
+https://pair-code.github.io/lit/
+
+
+
+RLHF: Reinforcement Learning by Human Feedback
+
+
+
+LLMOps:
+------------------------------------------------------------------------>
+
+1. Scalabillity:  Managing computational resources to handle varying workloads without promising performance.
+2. Latency Optimization: Reducing response times for real-time application through model optimization and efficient infrastructure
+3. Monitoring and Logging: Continuously Tracking model outputs for quality assurance and compliance, enabling rapid response to issues.
+4. Version Control and Updates: Maintaining consistent performance across model updates, with rollback mechanisms in case of unforseen problems
+
+API layer on top of apps - Caching Layer
+
+
+
