422bb82a2400d37030747dcda82854576a248ae4
Bharath Dasaraju <bhrth.dsra1@gmail.com>
2025-03-22 15:37:22 +0800

    LLMs performance

--- 14.Azure_GenAI_LLM_RAG/7.LLMs_performance.sh
+++ 14.Azure_GenAI_LLM_RAG/7.LLMs_performance.sh
@@ -1,2 +1,16 @@
 LLMs performance
 
+lmsys
+
+chatbot arena
+
+https://lmarena.ai/
+
+
+x.ai
+
+https://huggingface.co/spaces/lmarena-ai/chatbot-arena-leaderboard
+
+
+https://docs.ragas.io/en/stable/
+
