6ed758c63188c70e6ed04f04680635443193ff91
Bharath Dasaraju <bhrth.dsra1@gmail.com>
2025-03-25 05:44:30 +0800

    Amazon Bedrock - AI hosting platform in AWS

---
+++ 13.AWS-AI-Cert/2.amazon_bedrock.sh
@@ -0,0 +1,9 @@
+1. Amazon Bedrock Foundational Models(FM)
+2. Amazon Bedrock Fine-Tuning Model
+3. Amazon Bedrock RAG & Knowledge Base
+4. Amazon Bedrock GuardRails
+5. Amazon Bedrock Agents
+6. Amazon Bedrock - cloudwatch integration
+
+Amazon AIStylist - https://aistylist.awsplayer.com/
+
