50cb1b49fe2c181160d58bef02e4bfcf3f7f9792
Bharath Dasaraju <bhrth.dsra1@gmail.com>
2025-03-26 04:48:44 +0800

    RAG and Knowledgebase

--- 13.AWS-AI-Cert/2.amazon_bedrock.sh
+++ 13.AWS-AI-Cert/2.amazon_bedrock.sh
@@ -1,3 +1,5 @@
+GPT-4o is a foundational Model
+
 1. Amazon Bedrock Foundational Models(FM)
 2. Amazon Bedrock Fine-Tuning Model
 3. Amazon Bedrock RAG & Knowledge Base
@@ -6,4 +8,51 @@
 6. Amazon Bedrock - cloudwatch integration
 
 Amazon AIStylist - https://aistylist.awsplayer.com/
+
+
+Generative AI for images from text Diffusion Model(Stable Diffusion)
+
+
+
+1. Amazon Bedrock Foundational Models(FM)
+-------------------------------------------------
+RAG, LLM Agents etc..
+
+
+
+Automated Metrics to Evaluate the Foundational Model(FM):
+---------------------------------------------------------------------->
+
+ROUGE: Recall-Oriented Understudy for Gisting Evaluation   - this is for evaluating the summarization and machine translation systems.
+   ROUGE-N  - Matching the number of n-grams between reference text and generated text.
+   ROUGE-L  - Longest common subseuence between reference and generated text.
+
+BLEU: Bilingual Evaluation Understudy  -- Evaluate the quality of generated text, especially for translations.
+
+BERTScore: Bidirectional Encoder Representations from Transformers.....
+        Evaluates the Semantic simialrity between generated text.
+
+  Uses BERTScore to compare the contextualized embeddings of both texts and computes the cosine similarity between them.
+
+Perplexity: How well the model predicts the next token(Lower is better)
+
+Business Metrics:
+--------------------------------->
+user satisifaction
+Cross-domain performance
+average revenue per user
+
+
+Amazon Bedrock - RAG and Knowledge Base
+----------------------------------------------------->
+
+RAG - Retrieval Augmented Generation
+RAG allows a Foundation Model to reference a data source outside of its training data without being fine-tuned.
+
+So we have a knowledge base and its being built and managed by Amazon Bedrock.
+
+
+
+
+
 
