b5025d73c7dab45330c2783aea00e1691423b47a
Bharath Dasaraju <bhrth.dsra1@gmail.com>
2025-03-27 06:13:09 +0800

    ML Terms

---
+++ 13.AWS-AI-Cert/3.amazon_Q.sh
@@ -0,0 +1,34 @@
+Amazon Q - Business -----
+
+Fully Managed Gen-AI assitant for your business
+
+Amazon-Q -- fully managed RAG
+
+Amazon-Q - Admin Controls
+
+Amazon-Q Apps:
+
+Amazon-Q Developer: Similar to Github Copilot -- supports java, javascript, pythin , c# etc
+
+
+Amazon Q for AWS Services:
+------------------------------------->
+1. Amazon QuickSight
+2. Amazon Q for Ec2
+3. Amazon Q for AWS Chatbot
+3. Amazon Q for Glue
+
+
+PartyRock:
+----------->
+
+GenAI app-building playground powered by Amazon Bedrock - Allows you to experiment creating GenAI apps with various FMs
+
+https://partyrock.aws
+
+
+
+
+
+
+
