dc89deadea348ea5041aeb19d8f58356803e3104
Bharath Dasaraju <bhrth.dsra1@gmail.com>
2025-03-27 12:08:26 +0800

    ML Lifecycle policies

---
+++ 13.AWS-AI-Cert/4.ai-ml-dl.sh
@@ -0,0 +1,55 @@
+Ml- Algorithms
+
+Supervised Learning:
+
+1. Regression  - numeric value
+2. Classification  - categorical label of input data.
+
+
+Training Set
+Validation Set
+Test Set
+
+
+Feature Engineering:
+
+Feature Engineering on structured and unstructured data.
+
+The process of using domain knowledge to select and transform raw data into meaningful features.
+
+
+Supervised Learning and
+Unsupervised Learning
+
+
+Model Fit, Bias and Variance
+
+Model Evaluation metrics:
+--------------------------------->
+
+Acuuracy of classification models:
+1. PRecision
+2. Recall
+3. F1 Score
+4. Accuracy
+
+
+AUC-ROC --- Area Under Curve - Reciever Operator Curve
+
+
+Regression metrics
+
+MAE - Mean Absolute Error
+MAPE - Mean Absolute Percentage Error
+RMSE - Root Mean Squared Error
+R^2 -
+
+INferencing - is when a model is making prediction on new data.
+
+Realtime inferncing - chatbots
+Batch inferncing  - Data analytics
+INferencing at Edge  - LLM on remote server or run small language model(SLM)
+
+
+
+
---
+++ 13.AWS-AI-Cert/5.ml.sh
@@ -0,0 +1,5 @@
+Data collection and Preparation
+Feature Engineering
+Model Training and Parameter Tuning
+Model Evalution
+
