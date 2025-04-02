b589f2dd50a1076a8162456eae4205a5bad6b76b
Bharath Dasaraju <bhrth.dsra1@gmail.com>
2025-04-03 06:33:59 +0800

    Azure AI service enable diagnostic logging

--- 17.Azure_Ai102/1.Azure_AI_Solutions/Azure_AI/2.Azure_AI_Services.sh
+++ 17.Azure_Ai102/1.Azure_AI_Solutions/Azure_AI/2.Azure_AI_Services.sh
@@ -7,5 +7,14 @@ Language containers
 Speech containers
 Vision containers
 
+Diagnostic Logging provides insights into the operations of Azure-AI services.
+1. Activity Logs
+2. Resource Logs
+3. Metrics
+
+
+Log Analytics
+Azure Monitor  -- Azure Metrics
+Event Hubs
 
 
